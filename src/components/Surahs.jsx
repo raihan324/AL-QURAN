78444a147220417a53923e52ecc09d98a612a2e5
Raihan <raihanu600@gmail.com>
2022-09-29 17:25:41 +0600

    New Added

--- src/components/Surahs.jsx
+++ src/components/Surahs.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
+import classes from '../styles/Surahs.module.css'
 // import Surah from './Surah'
 
 const Surahs = ({number, name, englishName, englishNameTranslation, revelationType, ayahs}) => {
   return (
-    <div className="text__wrapper">
-        <div className="style__number">
-          <h3>{number}</h3>
-          <h3>{englishName}</h3>
-          <p>{englishNameTranslation}</p>
+    <div className={classes.text__wrapper}>
+        <div className={classes.style__number}>
+          <span>
+            <h3>{number}</h3>
+          </span>
+          <div>
+            <p></p>
+            <h3>{englishName}</h3>
+            <p>{englishNameTranslation}</p>
+          </div>
         </div>
-        <div className="arby__ayat">
+        <div className={classes.arby__ayat}>
           <h3>{name}</h3>
           <p>{ayahs.length} Ayahs</p>
         </div>
