533fd8b22346c33a9eb05bcd30a19c10d1561df4
Raihan <raihanu600@gmail.com>
2022-09-29 22:45:43 +0600

    Added new features

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import Home from './components/Home';
-import Test from './components/Test';
 
 function App() {
   return (
     <>
       <Home/>
-      <Test/>
     </>
   )
 }
